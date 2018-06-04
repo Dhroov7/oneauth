0d8bc88e8ce7f06191caf2f6dfe4d08c0c50c2e6
Dhroov Gupta <dhroovgupta7@gmail.com>
2018-06-04 19:52:27 +0530

    Stopped creating multiple accounts from same email

--- src/passport/strategies/user/strategy-facebook.js
+++ src/passport/strategies/user/strategy-facebook.js
@@ -25,95 +25,107 @@ module.exports = new FacebookStrategy({
     passReqToCallback: true,
 }, function (req, authToken, refreshToken, profile, cb) {
     let profileJson = profile._json
-    let oldUser = req.user
-    // DATADOG TRACE: START SPAN
-    Raven.setContext({extra: {file: 'fbstrategy'}})
-    const span = tracer.startSpan('passport.strategy.facebook')
-    if (oldUser) {
-        if (config.DEBUG)
-            console.log('User exists, is connecting Facebook account')
-        models.UserFacebook.findOne({where: {id: profileJson.id}})
-            .then((fbaccount) => {
+    models.User.findOne({
+        where:{
+            email:profileJson.email
+        }
+    }).then((userFacebook) => {
+        if(userFacebook){
+            return cb(null,false)
+        }
 
-                if (fbaccount) {
-                    throw new Error('Your Facebook account is already linked with codingblocks account Id: ' + fbaccount.dataValues.userId)
-                }
-                else {
-                    models.UserFacebook.upsert({
-                        id: profileJson.id,
-                        accessToken: authToken,
-                        refreshToken: refreshToken,
-                        photo: "https://graph.facebook.com/" + profileJson.id + "/picture?type=large",
-                        userId: oldUser.id
-                    })
-                        .then(function (updated) {
-                            return models.User.findById(oldUser.id)
+        let oldUser = req.user
+        // DATADOG TRACE: START SPAN
+        Raven.setContext({extra: {file: 'fbstrategy'}})
+        const span = tracer.startSpan('passport.strategy.facebook')
+        if (oldUser) {
+            if (config.DEBUG)
+                console.log('User exists, is connecting Facebook account')
+            models.UserFacebook.findOne({where: {id: profileJson.id}})
+                .then((fbaccount) => {
+
+                    if (fbaccount) {
+                        throw new Error('Your Facebook account is already linked with codingblocks account Id: ' + fbaccount.dataValues.userId)
+                    }
+                    else {
+                        models.UserFacebook.upsert({
+                            id: profileJson.id,
+                            accessToken: authToken,
+                            refreshToken: refreshToken,
+                            photo: "https://graph.facebook.com/" + profileJson.id + "/picture?type=large",
+                            userId: oldUser.id
                         })
-                        .then(function (user) {
-                            // DATADOG TRACE: END SPAN
-                            user.update({photo: "https://graph.facebook.com/" + profileJson.id + "/picture?type=large"})
-                            setImmediate(() => {
-                                span.addTags({
-                                    resource: req.path,
-                                    type: 'web',
-                                    'span.kind': 'server',
-                                    userId: oldUser.id,
-                                    newUser: false,
-                                    facebookId: profileJson.id
-                                })
-                                span.finish()
+                            .then(function (updated) {
+                                return models.User.findById(oldUser.id)
                             })
-                            return cb(null, user.get())
-                        })
-                        .catch((err) => {
-                            Raven.captureException(err)
-                            return cb(err, null)
-                        })
-                }
-            })
-            .catch((err) => {
-                Raven.captureException(err)
-                return cb(null, false, {message: err.message})
-            })
-    }
-    else {
-
-        models.UserFacebook.findCreateFind({
-            include: [models.User],
-            where: {id: profileJson.id},
-            defaults: {
-                id: profileJson.id,
-                accessToken: authToken,
-                refreshToken: refreshToken,
-                photo: "https://graph.facebook.com/" + profileJson.id + "/picture?type=large",
-                user: {
-                    username: profileJson.first_name + '-' + profileJson.last_name + '-' + profileJson.id,
-                    firstname: profileJson.first_name,
-                    lastname: profileJson.last_name,
-                    email: profileJson.email,
-                    photo: "https://graph.facebook.com/" + profileJson.id + "/picture?type=large"
-                }
-            }
-        }).spread(function (userFacebook, created) {
-            //TODO: Check 'created' == true to see if first time user
-            if (!userFacebook) {
-                return cb(null, false, {message: 'Authentication Failed'})
-            }
-            // DATADOG TRACE: END SPAN
-            setImmediate(() => {
-                span.addTags({
-                    resource: req.path,
-                    type: 'web',
-                    'span.kind': 'server',
-                    userId: userFacebook.user.id,
-                    newUser: true,
-                    facebookId: profileJson.id
+                            .then(function (user) {
+                                // DATADOG TRACE: END SPAN
+                                user.update({photo: "https://graph.facebook.com/" + profileJson.id + "/picture?type=large"})
+                                setImmediate(() => {
+                                    span.addTags({
+                                        resource: req.path,
+                                        type: 'web',
+                                        'span.kind': 'server',
+                                        userId: oldUser.id,
+                                        newUser: false,
+                                        facebookId: profileJson.id
+                                    })
+                                    span.finish()
+                                })
+                                return cb(null, user.get())
+                            })
+                            .catch((err) => {
+                                Raven.captureException(err)
+                                return cb(err, null)
+                            })
+                    }
                 })
-                span.finish()
-            })
-            return cb(null, userFacebook.user.get())
-        }).catch((err) => Raven.captureException(err))
-    }
+                .catch((err) => {
+                    Raven.captureException(err)
+                    return cb(null, false, {message: err.message})
+                })
+        }
+        else {
+
+            models.UserFacebook.findCreateFind({
+                include: [models.User],
+                where: {id: profileJson.id},
+                defaults: {
+                    id: profileJson.id,
+                    accessToken: authToken,
+                    refreshToken: refreshToken,
+                    photo: "https://graph.facebook.com/" + profileJson.id + "/picture?type=large",
+                    user: {
+                        username: profileJson.first_name + '-' + profileJson.last_name + '-' + profileJson.id,
+                        firstname: profileJson.first_name,
+                        lastname: profileJson.last_name,
+                        email: profileJson.email,
+                        photo: "https://graph.facebook.com/" + profileJson.id + "/picture?type=large"
+                    }
+                }
+            }).spread(function (userFacebook, created) {
+                //TODO: Check 'created' == true to see if first time user
+                if (!userFacebook) {
+                    return cb(null, false, {message: 'Authentication Failed'})
+                }
+                // DATADOG TRACE: END SPAN
+                setImmediate(() => {
+                    span.addTags({
+                        resource: req.path,
+                        type: 'web',
+                        'span.kind': 'server',
+                        userId: userFacebook.user.id,
+                        newUser: true,
+                        facebookId: profileJson.id
+                    })
+                    span.finish()
+                })
+                return cb(null, userFacebook.user.get())
+            }).catch((err) => Raven.captureException(err))
+        }
+    }).catch((err) => {
+        Raven.captureException(err)
+    })
 
 
 })
