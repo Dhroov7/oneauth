f6b05644bafa53a847804b17afc5a97faffbc75f
Dhroov Gupta <dhroovgupta7@gmail.com>
2018-08-07 11:03:16 +0530

    Remove the wrapper dunction

--- config.js
+++ config.js
@@ -1,10 +1,12 @@
+
 config = {
     "FACEBOOK_CALLBACK": "/login/facebook/callback",
     "FACEBOOK_LOGIN_SCOPES": ["email", "public_profile"],
     "TWITTER_CALLBACK": "/login/twitter/callback",
     "GITHUB_CALLBACK": "/login/github/callback",
     "GOOGLE_CALLBACK": "/login/google/callback",
-    "LINKEDIN_CALLBACK": "/connect/linkedin/callback", "GRANT_TOKEN_SIZE": 32,
+    "LINKEDIN_CALLBACK": "/connect/linkedin/callback",
+    "GRANT_TOKEN_SIZE": 32,
     "AUTH_TOKEN_SIZE": 64,
     "BCRYPT_SALT_ROUNDS": 8,
     "DEBUG": false,
@@ -12,11 +14,11 @@ config = {
     "EMAIL_SENDER_ADDR": "info@codingblocks.com",
     "WELCOME_EMAIL": "51913645-587b-419c-af40-79fc10553451",
     "FORGOT_USER_EMAIL": "a7e8ca83-0b5f-4ec8-b7cb-a3dbd2839c46",
-    "FORGOT_PASS_EMAIL": "64d80a73-194d-4988-a581-87fbcf5457c7",
-    "VERIFY_EMAIL": "3e470c46-5d2b-471e-992e-4820f1599de3"
-}
+    "FORGOT_PASS_EMAIL":"64d80a73-194d-4988-a581-87fbcf5457c7",
+    "VERIFY_EMAIL" :"3e470c46-5d2b-471e-992e-4820f1599de3"
+};
 
-config.DEPLOY_CONFIG = process.env.ONEAUTH_DEV || 'production'
+config.DEPLOY_CONFIG = process.env.ONEAUTH_DEV || 'production';
 
 switch (config.DEPLOY_CONFIG) {
 
@@ -25,7 +27,8 @@ switch (config.DEPLOY_CONFIG) {
         config.SERVER_URL = 'http://localhost:3838'
         config.DEBUG = true
         config.SECRETS = require('./secrets-sample.json')
-        break
+        break;
+
 
 
     case 'heroku':
@@ -38,19 +41,19 @@ switch (config.DEPLOY_CONFIG) {
         if (process.env.NEW_RELIC_LICENSE_KEY) {
             config.SECRETS.NEWRELIC_LICENSE_KEY = process.env.NEW_RELIC_LICENSE_KEY
         }
-        if (process.env.SENDGRID_API_KEY) {
+        if(process.env.SENDGRID_API_KEY) {
             config.SECRETS.SENDGRID_API_KEY = process.env.SENDGRID_API_KEY
         }
         config.NEWRELIC_LOG_LEVEL = 'trace'
-        break
+        break;
 
 
-    case 'production':
-    default:
-        config.SERVER_URL = 'https://account.codingblocks.com'
-        config.SECRETS = require('./secrets.json')
-        config.COOKIE_DOMAIN = '.codingblocks.com'
-        break
+
+    case 'production': default:
+    config.SERVER_URL = 'https://account.codingblocks.com'
+    config.SECRETS = require('./secrets.json')
+    config.COOKIE_DOMAIN = '.codingblocks.com'
+    break;
 }
 
-module.exports = config
+module.exports = config;
--- src/routers/connect/linkedin.js
+++ src/routers/connect/linkedin.js
@@ -4,19 +4,11 @@ const passport = require('../../passport/passporthandler')
 const config = require('../../../config')
 const debug = require('debug')('oauth:connect:linkedin')
 
-function AuthzLinkedin(req, res, next) {
-        if (config.DEBUG) debug("Authz Linkedin = = = = = = ")
-        passport.authorize('linkedin', {
-            //TODO: Add failure flash
-            failureRedirect: '/users/me',
-            failureFlash: true
-        })(req, res, next)
-}
 
 router.get('/', passport.authorize('linkedin'))
 
 
-router.get('/callback',AuthzLinkedin ,function (req,res) {
+router.get('/callback',passport.authorize('linkedin',{failureRedirect: '/users/me',failureFlash: true}) ,function (req,res) {
     res.redirect('/users/me')
 })
 
