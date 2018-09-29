26ee0f9b6ad62a0fb729da39804c1ec6fb0b5149
Dhroov Gupta <dhroovgupta7@gmail.com>
2018-09-30 02:18:30 +0530

    remove debug

--- src/passport/strategies/user/strategy-linkedin.js
+++ src/passport/strategies/user/strategy-linkedin.js
@@ -5,7 +5,6 @@ const models = require('../../../db/models').models
 const config = require('../../../../config')
 const secrets = config.SECRETS
 
-const debug = require('debug')('oauth:strategy:linkedin')
 
 module.exports = new LinkedinStrategy({
     clientID: secrets.LINKEDIN_CLIENT_ID,
