aac9abdf92ab3e7568eea06c993fb1274a1cb815
Dhroov Gupta <dhroovgupta7@gmail.com>
2018-09-30 02:36:31 +0530

    remove debug

--- src/routers/connect/linkedin.js
+++ src/routers/connect/linkedin.js
@@ -2,7 +2,6 @@ const router = require('express').Router()
 const passport = require('../../passport/passporthandler')
 
 const config = require('../../../config')
-const debug = require('debug')('oauth:connect:linkedin')
 
 
 router.get('/', passport.authorize('linkedin'))
